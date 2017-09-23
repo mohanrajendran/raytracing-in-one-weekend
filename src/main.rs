cb3c9ff34ed0f8f8de0b0a41979261ddc967a212
Mohan Rajendran <lynermohan@gmail.com>
2017-09-23 16:40:44 -0500

    Created gradient image

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 extern crate image;
 
+mod ray;
 mod vec3;
 
 use std::fs::File;
 use std::path::Path;
 
+use ray::Ray;
 use vec3::Vec3;
+
+fn color(ray: &Ray) -> Vec3 {
+    let dir = ray.direction().normal();
+    let t = 0.5*(dir.y() + 1.0);
+    Vec3::new(1.0, 1.0, 1.0) * (1.0-t) + Vec3::new(0.5, 0.7, 1.0) * t
+}
 
 fn main() {
     let width = 200;
     let height = 100;
 
+    let lower_left_corner = Vec3::new(-2.0, -1.0, 1.0);
+    let horizontal = Vec3::new(4.0, 0.0, 0.0);
+    let vertical = Vec3::new(0.0, 2.0, 0.0);
+    let origin = Vec3::new(0.0, 0.0, 0.0);
     let mut image_buf = image::ImageBuffer::new(width, height);
 
     for (x, y, pixel) in image_buf.enumerate_pixels_mut() {
@@ -18,8 +30,11 @@ fn main() {
         let y = (height - y) as f32;
         let w = width as f32;
         let h = height as f32;
-        let color = Vec3::new(x/w, y/h, 0.2);
-        *pixel = image::Rgb(color.rgb());
+        let u = x/w;
+        let v = y/h;
+        let r = Ray::new(origin, lower_left_corner + horizontal*u + vertical*v);
+        let col = color(&r);
+        *pixel = image::Rgb(col.rgb());
     }
 
     let ref mut fout = File::create(&Path::new("image.png")).unwrap();
--- src/vec3.rs
+++ src/vec3.rs
@@ -1,5 +1,7 @@
-#![allow(dead_code)]
-#[derive(Debug)]
+use std::f32;
+use std::ops::{Add, Mul};
+
+#[derive(Debug, Clone, Copy)]
 pub struct Vec3(f32, f32, f32);
 
 impl Vec3 {
@@ -20,11 +22,39 @@ impl Vec3 {
     pub fn z(&self) -> f32 {
         self.2
     }
+    pub fn length(&self) -> f32 {
+        (self.0 * self.0 + self.1 * self.1 + self.2 * self.2).sqrt()
+    }
+
+    pub fn normal(&self) -> Vec3 {
+        let len = self.length();
+        if len == 0.0 {
+            Vec3::new(0.0,0.0,0.0)
+        } else {
+            *self * (1.0 / len)
+        }
+    }
 
     pub fn rgb(&self) -> [u8; 3] {
         let r = (255.9 * self.0) as u8;
         let g = (255.9 * self.1) as u8;
         let b = (255.9 * self.2) as u8;
         [r, g, b]
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, other: Vec3) -> Vec3 {
+        Vec3::new(self.0 + other.0, self.1 + other.1, self.2 + other.2)
+    }
+}
+
+impl Mul<f32> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, m: f32) -> Vec3 {
+        Vec3::new(self.0 * m, self.1 * m, self.2 * m)
     }
 }
